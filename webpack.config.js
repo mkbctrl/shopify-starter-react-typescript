122824ba2f29ccc52d90944968fbe36aab02099d
Mikołaj Koropecki <mikolaj.koropecki@ragnarson.com>
2020-04-08 22:12:44 +0200

    feat: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const APP_DIR = path.resolve(__dirname, 'src')
 
 module.exports = {
+  mode: 'production',
   entry: `${APP_DIR}/scripts/common.js`,
   output: {
     path: path.resolve(__dirname, 'assets'),
