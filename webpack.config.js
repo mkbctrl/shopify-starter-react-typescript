e68b62746d6e275c4222c3a2d0b706df2823182a
Mikołaj Koropecki <mikolaj.koropecki@ragnarson.com>
2020-04-08 18:37:47 +0200

    feat: add yarn

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const APP_DIR = path.resolve(__dirname, 'src')
+
+module.exports = {
+  entry: `${APP_DIR}/scripts/common.js`,
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'application.js'
+  }
+}
